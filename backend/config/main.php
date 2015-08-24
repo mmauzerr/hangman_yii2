bb96b679c06de5cd4ced443e9a778378797e6b52
mmauzerr <mmauzerr@gmail.com>
2015-08-24 12:52:22 +0200

    setting correction

--- backend/config/main.php
+++ backend/config/main.php
@@ -17,6 +17,17 @@ return [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
         ],
+        'urlManager' => [ 'class' => 'yii\web\UrlManager',
+            'showScriptName' => false,
+            'enablePrettyUrl' => true,
+            'rules' => [
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+                '<controller:\w+\-\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<id:\d+>/<slug:[A-Za-z0-9\-_\.\/]+>' => '<controller>/view',
+            ],
+        ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
